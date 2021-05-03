18dd6f9cadeab3e89145830de303b2bdad3baeb9
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2021-05-03 03:36:50 +0200

    Used logger ^^

--- src/main/java/net/griefergames/reloaded/exception/ExceptionHandler.java
+++ src/main/java/net/griefergames/reloaded/exception/ExceptionHandler.java
@@ -1,6 +1,9 @@
 package net.griefergames.reloaded.exception;
 
 import lombok.val;
+import net.griefergames.reloaded.utils.logger.GrieferGamesLogger;
+
+import java.util.logging.Level;
 
 public class ExceptionHandler {
 
@@ -12,6 +15,6 @@ public class ExceptionHandler {
 
         final int lineNumber = stackTraceElement.getLineNumber();
 
-        System.out.println( message + ": {" + className + " - " + methodName + " - " + lineNumber + "} - " + exception );
+        GrieferGamesLogger.log( Level.WARNING, "{0}: {{1} - {2} - {3}} - {4}", message, className, methodName, lineNumber, exception );
     }
 }
