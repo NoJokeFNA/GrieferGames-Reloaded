73e81df961dbcd93aefc4487ac1a82662f978938
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2021-05-04 03:26:13 +0200

    Changed Java version to Java 11 Added "executeQuery" to HikariSqlFactory - still in beta

--- src/main/java/net/griefergames/reloaded/database/factory/HikariSqlFactory.java
+++ src/main/java/net/griefergames/reloaded/database/factory/HikariSqlFactory.java
@@ -1,15 +1,27 @@
 package net.griefergames.reloaded.database.factory;
 
 import lombok.NonNull;
-import lombok.Setter;
+import net.griefergames.reloaded.database.DataSource;
+import net.griefergames.reloaded.exception.ExceptionHandler;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.function.Consumer;
 
 public abstract class HikariSqlFactory {
 
-    @Setter
-    @NonNull
-    private String table;
-
-    public void keyExists() {
-
+    /**
+     * Execute a query in your database and get directly the result by the {@code preparedStatementConsumer}
+     *
+     * @param sqlQuery                  the full sql query
+     * @param preparedStatementConsumer the {@link PreparedStatement} callback
+     */
+    public void executeQuery( @NonNull final String sqlQuery, @NonNull final Consumer<PreparedStatement> preparedStatementConsumer ) {
+        try ( Connection connection = DataSource.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement( sqlQuery ) ) {
+            preparedStatementConsumer.accept( preparedStatement );
+        } catch ( SQLException exception ) {
+            ExceptionHandler.handleException( exception, "Error while checking whether a key exists or not" );
+        }
     }
 }
