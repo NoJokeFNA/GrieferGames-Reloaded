60f84a6e3212ebc9bf9473fd3253d0568e68d597
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2021-05-05 05:48:08 +0200

    Cleaned up code

--- src/main/java/net/griefergames/reloaded/utils/prefix/impl/CloudNetV2Prefix.java
+++ src/main/java/net/griefergames/reloaded/utils/prefix/impl/CloudNetV2Prefix.java
@@ -17,12 +17,14 @@ public class CloudNetV2Prefix implements IPrefix {
                 .getPermissionEntity()
                 .getHighestPermissionGroup( CloudAPI.getInstance().getPermissionPool() );
 
+        final String playerName = player.getName();
+
         switch ( "asd" ) {
             case "display":
-                return ChatUtil.sendColoredMessage( permissionEntity.getDisplay() + player.getName() );
+                return ChatUtil.sendColoredMessage( permissionEntity.getDisplay() + playerName );
 
             case "prefix":
-                return ChatUtil.sendColoredMessage( permissionEntity.getPrefix() + player.getName() );
+                return ChatUtil.sendColoredMessage( permissionEntity.getPrefix() + playerName );
 
             default:
                 throw new UnsupportedOperationException( "§4Unsupported value in §cchat_settings.yml » chat.cloudnet.use » " + "asd" );
--- src/main/java/net/griefergames/reloaded/utils/prefix/impl/CloudNetV3Prefix.java
+++ src/main/java/net/griefergames/reloaded/utils/prefix/impl/CloudNetV3Prefix.java
@@ -1,8 +1,10 @@
 package net.griefergames.reloaded.utils.prefix.impl;
 
-import lombok.NonNull;
 import de.dytanic.cloudnet.driver.CloudNetDriver;
 import de.dytanic.cloudnet.driver.permission.IPermissionGroup;
+import de.dytanic.cloudnet.driver.permission.IPermissionManagement;
+import de.dytanic.cloudnet.driver.permission.IPermissionUser;
+import lombok.NonNull;
 import net.griefergames.reloaded.utils.chat.ChatUtil;
 import net.griefergames.reloaded.utils.prefix.IPrefix;
 import org.bukkit.entity.Player;
@@ -11,17 +13,23 @@ public class CloudNetV3Prefix implements IPrefix {
 
     @Override
     public String getPrefix( @NonNull Player player ) {
-        final IPermissionGroup iPermissionGroup = CloudNetDriver
-                .getInstance()
-                .getPermissionManagement()
-                .getHighestPermissionGroup( CloudNetDriver.getInstance().getPermissionManagement().getUser( player.getUniqueId() ) );
+        final CloudNetDriver cloudNetDriver = CloudNetDriver.getInstance();
+
+        final IPermissionManagement iPermissionManagement = cloudNetDriver.getPermissionManagement();
+        final IPermissionUser iPermissionUser = iPermissionManagement.getUser( player.getUniqueId() );
+        if ( iPermissionUser == null )
+            return "User cannot be null";
+
+        final IPermissionGroup iPermissionGroup = iPermissionManagement.getHighestPermissionGroup( iPermissionUser  );
+
+        final String playerName = player.getName();
 
         switch ( "asd" ) {
             case "display":
-                return ChatUtil.sendColoredMessage( iPermissionGroup.getDisplay() + player.getName() );
+                return ChatUtil.sendColoredMessage( iPermissionGroup.getDisplay() + playerName );
 
             case "prefix":
-                return ChatUtil.sendColoredMessage( iPermissionGroup.getPrefix() + player.getName() );
+                return ChatUtil.sendColoredMessage( iPermissionGroup.getPrefix() + playerName );
 
             default:
                 throw new UnsupportedOperationException( "§4Unsupported value in §cchat_settings.yml » chat.cloudnet.use » " + "" );
