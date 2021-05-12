fee6a8ae68ce5514ec9fd5465418db9407406e08
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2021-05-12 15:13:44 +0200

    Added PlayerHolographicController

---
+++ src/main/java/net/griefergames/reloaded/modules/holographic/PlayerHolographicController.java
@@ -0,0 +1,41 @@
+package net.griefergames.reloaded.modules.holographic;
+
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import lombok.NonNull;
+import net.griefergames.reloaded.GrieferGamesReloaded;
+import org.bukkit.entity.Player;
+
+public class PlayerHolographicController {
+
+    public void createHolographic( @NonNull final Player player, final int holographicId, @NonNull final String text ) {
+        //TODO: Set the players holographic -> Database
+
+        final Hologram hologram = HologramsAPI.createHologram( GrieferGamesReloaded.PLUGIN.getPlugin(), player.getLocation() );
+        hologram.appendTextLine( text );
+    }
+
+    public void listHolographs( @NonNull final Player player ) {
+        //TODO: Get holographs from the player -> Database
+    }
+
+    public void addLine( @NonNull final Player player, final int holographicId, @NonNull final String text ) {
+        //TODO: Set the players holographic -> Database
+    }
+
+    public void editLine( @NonNull final Player player, final int holographicId, final int lineNumber, @NonNull final String text ) {
+
+    }
+
+    public void moveHolographic( @NonNull final Player player, final int holographicId ) {
+
+    }
+
+    public void deleteHolographic( @NonNull final Player player, final int holographicId ) {
+
+    }
+
+    public void sendHelpMessage( @NonNull final Player player ) {
+
+    }
+}
