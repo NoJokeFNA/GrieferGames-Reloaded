99d66cf143c7a0b1b2d5ab455ab15dd9d4b0f320
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2021-06-06 21:25:01 +0200

    Just some changes

--- src/main/java/net/griefergames/reloaded/modules/holographic/PlayerHolographicController.java
+++ src/main/java/net/griefergames/reloaded/modules/holographic/PlayerHolographicController.java
@@ -1,9 +1,12 @@
 package net.griefergames.reloaded.modules.holographic;
 
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
 import net.griefergames.reloaded.GrieferGamesReloaded;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
 
 public class PlayerHolographicController {
 
@@ -19,22 +22,57 @@ public class PlayerHolographicController {
     }
 
     public void addLine( @NotNull final Player player, final int holographicId, @NotNull final String text ) {
-        //TODO: Set the players holographic -> Database
+        //TODO: Add a line from the players holograms line -> Database
+
+        final var hologramCollection = this.getHolograms();
+        hologramCollection.forEach( hologram -> {
+            //TODO: Do some stuff
+        } );
+    }
+
+    public void removeLine( @NotNull final Player player, final int holographicId, @NotNull final String text ) {
+        //TODO: Remove a line from the players holograms line -> Database
+
+        final var hologramCollection = this.getHolograms();
+        hologramCollection.forEach( hologram -> {
+            //TODO: Do some stuff
+            hologram.removeLine( holographicId );
+        } );
     }
 
     public void editLine( @NotNull final Player player, final int holographicId, final int lineNumber, @NotNull final String text ) {
+        //TODO: Edit the players holographic -> Database
 
+        final var hologramCollection = this.getHolograms();
+        hologramCollection.forEach( hologram -> {
+            //TODO: Do some stuff
+        } );
     }
 
     public void moveHolographic( @NotNull final Player player, final int holographicId ) {
+        //TODO: Move the players holographic -> Database
 
+        final var hologramCollection = this.getHolograms();
+        hologramCollection.forEach( hologram -> {
+            //TODO: Do some stuff
+        } );
     }
 
     public void deleteHolographic( @NotNull final Player player, final int holographicId ) {
+        //TODO: Delete the players holographic -> Database
 
+        final var hologramCollection = this.getHolograms();
+        hologramCollection.forEach( hologram -> {
+            //TODO: Do some stuff
+            hologram.delete();
+        } );
     }
 
     public void sendHelpMessage( @NotNull final Player player ) {
+        //TODO: Send help message to the player
+    }
 
+    private Collection<Hologram> getHolograms() {
+        return HologramsAPI.getHolograms( GrieferGamesReloaded.PLUGIN.getPlugin() );
     }
 }
