169e4c5cd9ebc2550e6e2226649d928ee79b5b0c
Julian <53055251+NoJokeFNA@users.noreply.github.com>
2021-06-06 21:18:33 +0200

    Cleaned up updateCache method

--- src/main/java/net/griefergames/reloaded/cache/CacheMethods.java
+++ src/main/java/net/griefergames/reloaded/cache/CacheMethods.java
@@ -1,36 +1,35 @@
 package net.griefergames.reloaded.cache;
 
 import net.griefergames.reloaded.GrieferGamesReloaded;
+import net.griefergames.reloaded.utils.logger.GrieferGamesLogger;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 public class CacheMethods {
 
-    public static void updateCache() {
-        final var timer = new Timer( true );
-        final var timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                // Execute methods
-                System.out.println( "Updating cache..." );
+	public static void updateCache() {
+		final Runnable cacheRunnable = () -> {
+			// Execute methods
+			GrieferGamesLogger.log( Level.INFO, "Updating cache", null );
 
-                // After executing queries into the database
-                clearCache();
-            }
-        };
+			// After executing queries into the database
+			clearCache();
+		};
 
-        timer.schedule( timerTask, 1000L * 60L * 15L, 1000L * 60L * 15L );
-    }
+		final var scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+		scheduledExecutorService.scheduleAtFixedRate( cacheRunnable, 15L, 15L, TimeUnit.MINUTES );
+	}
 
-    private static void clearCache() {
-        for ( final var cacheUuid : CacheHandler.USER_MAP.keySet() ) {
-            final var user = CacheHandler.getUserByUuid( cacheUuid );
-            final var player = GrieferGamesReloaded.PLUGIN.getPlugin().getServer().getPlayer( user.getPlayerUuid() );
-            if ( player == null )
-                CacheHandler.USER_MAP.remove( user.getPlayerUuid() );
+	private static void clearCache() {
+		for ( final var cacheUuid : CacheHandler.USER_MAP.keySet() ) {
+			final var user = CacheHandler.getUserByUuid( cacheUuid );
+			final var player = GrieferGamesReloaded.PLUGIN.getPlugin().getServer().getPlayer( user.getPlayerUuid() );
+			if ( player == null )
+				CacheHandler.USER_MAP.remove( user.getPlayerUuid() );
 
-            System.out.println( "Current cached players: " + CacheHandler.USER_MAP.size() );
-        }
-    }
+			GrieferGamesLogger.log( Level.INFO, "Current cached players: {0}", new Object[] { CacheHandler.USER_MAP.size() } );
+		}
+	}
 }
